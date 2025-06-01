01d4edf35fd6447ae1aa9e4a1aeefe58b4d0e30e
Le Tien Dat <letiendat22071999@gmail.com>
2025-06-01 21:48:53 +0700

    Build Reusable BasePage class - Part I

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -1,4 +1,121 @@
 package core;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
+
 public class BasePage {
+    //  1 - Access Modifier: public/ protected/ private/ default
+
+    //  2 - Kiểu dữ liệu của hàm (Data type): void/ int/ String/ boolean/ WebElement/ List<WebElement>/..
+    //  Nó sẽ liên quan đến cái chức năng mình viết trong thân hàm
+    // Dùng cái hàm nào của Selenium thì nó trả về cái gì -> Define kiểu dữ liệu đó
+
+    //  3 - Tên hàm: Đặt tên có ý nghĩa theo chức năng đang cần viết
+    //  Convention tuân theo chuẩn của từng ngôn ngữ lập trình (Java)
+    //  lowerCamelCase: từ đầu tiên viết thường - chữ cái đầu tiên của các từ tiếp theo sẽ viết hoa
+
+    //  4 - Có tham số hay ko (tùy vào chức năng cần viết)
+    // Dùng hàm nào của Selenium
+
+    //  5 - Kiểu dữ liệu trả về cho hàm
+    //  Nếu như có return dữ liệu thì sẽ khớp vs kiểu dữ liệu ở số 2
+    //  Nếu như có return thì nó là cái step cuối cùng
+
+    public void openUrl(WebDriver driver, String pageUrl) {
+        driver.get(pageUrl);
+    }
+
+    public String getPageTitle(WebDriver driver) {
+        return driver.getTitle();
+    }
+
+    public String getPageUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageSource(WebDriver driver) {
+        return driver.getPageSource();
+    }
+
+    public void backToPage(WebDriver driver) {
+        driver.navigate().back();
+    }
+
+    public void forwardToPage(WebDriver driver) {
+        driver.navigate().forward();
+    }
+
+    public void refreshPage(WebDriver driver) {
+        driver.navigate().refresh();
+    }
+
+    private Alert waitAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void acceptAlert(WebDriver driver) {
+        waitAlertPresence(driver).accept();
+    }
+
+    public void cancelAlert(WebDriver driver) {
+        waitAlertPresence(driver).dismiss();
+    }
+
+    public String getTextAlert(WebDriver driver) {
+        return waitAlertPresence(driver).getText();
+    }
+
+    public void sendkeyToAlert(WebDriver driver, String keysToSend) {
+        waitAlertPresence(driver).sendKeys(keysToSend);
+    }
+
+    public void sleepInSeconds(int timeInSeconds) {
+        try {
+            Thread.sleep(timeInSeconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(expectedTitle)) {
+                sleepInSeconds(3);
+                break;
+            }
+        }
+    }
+
+    public void switchToWindowByID(WebDriver driver, String windowID) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            if (!window.equals(windowID)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+    }
+
+    public void closAllExceptMain(WebDriver driver, String windowID) {
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            if (!window.equals(windowID)) {
+                driver.switchTo().window(window);
+                driver.close();
+            }
+        }
+
+        driver.switchTo().window(windowID);
+    }
 }
