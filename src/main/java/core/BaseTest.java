c401bfc2f6abd9c14e9fa048228a1a3b1503cda6
Le Tien Dat <letiendat22071999@gmail.com>
2025-06-09 12:19:31 +0700

    Level 03 - Multiple Browser and BaseTest

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,4 +1,34 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BaseTest {
+    WebDriver driver;
+    protected WebDriver getBrowserDriver(String appUrl, String browserName){
+        switch (browserName){
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser name is not valid!");
+        }
+
+        driver.get(appUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        return driver;
+    }
 }
---
+++ src/test/java/orangehrm/user/Login_03_Multiple_Browser.java
@@ -0,0 +1,78 @@
+package orangehrm.user;
+
+import core.BasePage;
+import core.BaseTest;
+import javaSDET.Topic_01_Keywords;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Login_03_Multiple_Browser extends BaseTest {
+    private WebDriver driver;
+    private BasePage basePage;
+    private String appUrl;
+
+    @Parameters({"appUrl", "browser"})
+    @BeforeClass
+    public void beforeClass(String appUrl, String browserName) {
+        this.appUrl = appUrl;
+        basePage = BasePage.getInstance();
+        driver = getBrowserDriver(appUrl, browserName);
+    }
+
+    @Test
+    public void Login_01_Empty() {
+        basePage.sendkeyToElement(driver, "//input[@name='username']", "");
+        basePage.sendkeyToElement(driver, "//input[@name='password']", "");
+        basePage.clickToElement(driver, "//button[contains(@class, 'orangehrm-login-button')]");
+
+        Assert.assertEquals(basePage.getElementText(driver, "//input[@name='username']/parent::div/following-sibling::span"), "Required");
+        Assert.assertEquals(basePage.getElementText(driver, "//input[@name='password']/parent::div/following-sibling::span"), "Required");
+    }
+
+    @Test
+    public void Login_02_Invalid_Username() {
+        basePage.sendkeyToElement(driver, "//input[@name='username']", "dat@gmail.com");
+        basePage.sendkeyToElement(driver, "//input[@name='password']", "admin123");
+        basePage.clickToElement(driver, "//button[contains(@class, 'orangehrm-login-button')]");
+
+        Assert.assertEquals(basePage.getElementText(driver, "//div[@class='orangehrm-login-error']//p[contains(@class, 'oxd-alert-content-text')]"), "Invalid credentials");
+    }
+
+    @Test
+    public void Login_03_Invalid_Password() {
+        basePage.sendkeyToElement(driver, "//input[@name='username']", "Admin");
+        basePage.sendkeyToElement(driver, "//input[@name='password']", "12345!@#$");
+        basePage.clickToElement(driver, "//button[contains(@class, 'orangehrm-login-button')]");
+
+        Assert.assertEquals(basePage.getElementText(driver, "//div[@class='orangehrm-login-error']//p[contains(@class, 'oxd-alert-content-text')]"), "Invalid credentials");
+    }
+
+    @Test
+    public void Login_04_Valid_Username_Password() {
+        basePage.sendkeyToElement(driver, "//input[@name='username']", "Admin");
+        basePage.sendkeyToElement(driver, "//input[@name='password']", "admin123");
+        basePage.clickToElement(driver, "//button[contains(@class, 'orangehrm-login-button')]");
+
+        Assert.assertTrue(isAllLoadingIconInvisible());
+        Assert.assertEquals(basePage.getElementText(driver, "//div[@class='oxd-topbar-header-title']//h6"), "Dashboard");
+    }
+
+    public boolean isAllLoadingIconInvisible() {
+        return basePage.waitListElementInvisible(driver, "//div[@class='oxd-loading-spinner']");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
