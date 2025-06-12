575e9cf7827096027bb98399770e34e2db7c6b9f
Le Tien Dat <letiendat22071999@gmail.com>
2025-06-12 15:51:07 +0700

    Apply Enum for Multiple Browser

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,15 +12,19 @@ import java.time.Duration;
 public class BaseTest {
     WebDriver driver;
     protected WebDriver getBrowserDriver(String appUrl, String browserName){
-        switch (browserName){
-            case "Firefox":
+        BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
+        switch (browserList){
+            case FIREFOX:
                 driver = new FirefoxDriver();
                 break;
-            case "Chrome":
+            case CHROME:
                 driver = new ChromeDriver();
                 break;
-            case "Edge":
+            case EDGE:
                 driver = new EdgeDriver();
+                break;
+            case SAFARI:
+                driver = new SafariDriver();
                 break;
             default:
                 throw new RuntimeException("Browser name is not valid!");
